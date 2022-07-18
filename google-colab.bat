4622f7edde36fe71ef6ce7a943197d5e4a44ab18
morokoshi-head <ry111000000@gmail.com>
2022-07-18 23:44:17 +0900

    接続回数カウント処理を修正

--- google-colab.bat
+++ google-colab.bat
@@ -1,16 +1,16 @@
 @echo off
 
 setlocal enabledelayedexpansion
-
 set max_h=12
-set wait_s=3600
+set wait_s=1200
+set /a count_num=%max_h%*(3600/%wait_s%)
 set url=https://colab.research.google.com/drive/1FfTkeYY3XLIygvmy_t0CPIc0dixuLGNe
 set file=google-colab.log
 
 if exist %file% del %file%
 
 for /l %%i in (1,1,%max_h%) do (
-  set log=^[%date% %time%^] Connect %%i/%max_h%
+  set log=^[!date! !time!^] Connect %%i/%count_num%
   start %url%
   echo !log!
   echo !log! >> %file%
