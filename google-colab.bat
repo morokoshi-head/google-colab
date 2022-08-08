117e6d6ec8350cecf5f30ea039e93dbbe7ab4be5
morokoshi-head <ry111000000@gmail.com>
2022-08-08 19:00:04 +0900

    ディレイ範囲を最小限にする

--- google-colab.bat
+++ google-colab.bat
@@ -1,6 +1,5 @@
 @echo off
 
-setlocal enabledelayedexpansion
 set max_h=12
 set wait_s=1200
 set /a count_num=%max_h%*(3600/%wait_s%)
@@ -9,6 +8,7 @@ set file=google-colab.log
 
 if exist %file% del %file%
 
+setlocal enabledelayedexpansion
 for /l %%i in (1,1,%max_h%) do (
   set log=^[!date! !time!^] Connect %%i/%count_num%
   start %url%
