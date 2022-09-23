e47200ad234e23245f821261f40f71004f9de396
morokoshi-head <ry111000000@gmail.com>
2022-09-24 01:31:03 +0900

    コードを修正

--- google-colab.bat
+++ google-colab.bat
@@ -1,7 +1,7 @@
 @echo off
 
 set max_h=12
-set wait_s=1200
+set wait_s=600
 set /a count_num=%max_h%*(3600/%wait_s%)
 set url=https://colab.research.google.com/drive/1FfTkeYY3XLIygvmy_t0CPIc0dixuLGNe
 set file=google-colab.log
@@ -9,7 +9,7 @@ set file=google-colab.log
 if exist %file% del %file%
 
 setlocal enabledelayedexpansion
-for /l %%i in (1,1,%max_h%) do (
+for /l %%i in (1,1,%count_num%) do (
   set log=^[!date! !time!^] Connect %%i/%count_num%
   start %url%
   echo !log!
